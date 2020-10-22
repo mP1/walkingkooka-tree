9a7717ac251b5dcf160f9d741aeaabd94aaf959e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-10-22 11:41:17 +1100

    ExpressionNumberReducer

---
+++ src/main/java/walkingkooka/tree/expression/ExpressionNumberReducerAddBinaryExpressionNumberVisitor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+final class ExpressionNumberReducerAddBinaryExpressionNumberVisitor extends ExpressionNumberReducerBinaryExpressionNumberVisitor {
+
+    static Number add(final Number left,
+                      final Number right,
+                      final ExpressionNumberReducerContext context) {
+        final ExpressionNumberReducerAddBinaryExpressionNumberVisitor visitor = new ExpressionNumberReducerAddBinaryExpressionNumberVisitor(context);
+        visitor.accept(left, right);
+        return visitor.result;
+    }
+
+    ExpressionNumberReducerAddBinaryExpressionNumberVisitor(final ExpressionNumberReducerContext context) {
+        super(context);
+    }
+
+    @Override
+    protected void visit(final BigDecimal left, final BigDecimal right) {
+        this.result = left.add(right, this.context.mathContext());
+    }
+
+    @Override
+    protected void visit(final BigInteger left, final BigInteger right) {
+        this.result = left.add(right);
+    }
+
+    @Override
+    protected void visit(final Double left, final Double right) {
+        this.result = left + right;
+    }
+
+    @Override
+    protected void visit(final Long left, final Long right) {
+        this.result = left + right; // TODO handle overflow https://github.com/mP1/walkingkooka-tree/issues/63
+    }
+
+    @Override
+    public String toString() {
+        return "add";
+    }
+}
---
+++ src/test/java/walkingkooka/tree/expression/ExpressionNumberReducerAddBinaryExpressionNumberVisitorTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression;
+
+public final class ExpressionNumberReducerAddBinaryExpressionNumberVisitorTest extends ExpressionNumberReducerBinaryExpressionNumberVisitorTestCase2<ExpressionNumberReducerAddBinaryExpressionNumberVisitor> {
+
+    @Override
+    public ExpressionNumberReducerAddBinaryExpressionNumberVisitor createVisitor() {
+        return new ExpressionNumberReducerAddBinaryExpressionNumberVisitor(CONTEXT);
+    }
+
+    @Override
+    int left() {
+        return 1;
+    }
+
+    @Override
+    int right() {
+        return 2;
+    }
+
+    @Override
+    int expected() {
+        return 3;
+    }
+
+    @Override
+    Number function(final Number left, final Number right, final ExpressionNumberReducerContext context) {
+        return ExpressionNumberReducerAddBinaryExpressionNumberVisitor.add(left, right, context);
+    }
+
+    @Override
+    String expectedToString() {
+        return "add";
+    }
+
+    @Override
+    public Class<ExpressionNumberReducerAddBinaryExpressionNumberVisitor> type() {
+        return ExpressionNumberReducerAddBinaryExpressionNumberVisitor.class;
+    }
+}
