a62964e39c88e6cb7b729c7fb7d201782a0e78b7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-10-23 11:48:27 +1100

    ExpressionNumberReducer.negate

---
+++ src/test/java/walkingkooka/tree/expression/ExpressionNumberReducerExpressionNumberVisitorNegateTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression;
+
+public final class ExpressionNumberReducerExpressionNumberVisitorNegateTest extends ExpressionNumberReducerExpressionNumberVisitorTestCase<ExpressionNumberReducerExpressionNumberVisitorNegate> {
+
+    @Override
+    public ExpressionNumberReducerExpressionNumberVisitorNegate createVisitor() {
+        return new ExpressionNumberReducerExpressionNumberVisitorNegate(CONTEXT);
+    }
+
+    @Override
+    int value() {
+        return -123;
+    }
+
+    @Override
+    int expected() {
+        return 123;
+    }
+
+    @Override
+    Number function(final Number value, final ExpressionNumberReducerContext context) {
+        return ExpressionNumberReducerExpressionNumberVisitorNegate.compute(value, context);
+    }
+
+    @Override
+    String expectedToString() {
+        return "negate";
+    }
+
+    @Override
+    public Class<ExpressionNumberReducerExpressionNumberVisitorNegate> type() {
+        return ExpressionNumberReducerExpressionNumberVisitorNegate.class;
+    }
+}
