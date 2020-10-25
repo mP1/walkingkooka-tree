bf0df4a6d77a27a9016a44296b8dc95dce73784f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-10-25 13:22:46 +1100

    ExpressionNumberConverterFromExpressionNumberNumber wrapped Converter support

---
+++ src/main/java/walkingkooka/tree/expression/ExpressionNumberConverterFromExpressionNumberNumber.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression;
+
+import walkingkooka.Either;
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterContext;
+
+import java.util.Objects;
+
+/**
+ * A {@link Converter} that wraps another {@link Converter} that is converting {@link Number numbers} to another type.
+ * This adds support for {@link ExpressionNumber} values.
+ */
+final class ExpressionNumberConverterFromExpressionNumberNumber implements Converter {
+
+    /**
+     * Wraps another {@link Converter}, which will receive the actual value of the {@link ExpressionNumber}.
+     */
+    static final ExpressionNumberConverterFromExpressionNumberNumber with(final Converter converter) {
+        Objects.requireNonNull(converter, "converter");
+
+        return new ExpressionNumberConverterFromExpressionNumberNumber(converter);
+    }
+
+    /**
+     * Use Singleton
+     */
+    private ExpressionNumberConverterFromExpressionNumberNumber(final Converter converter) {
+        super();
+        this.converter = converter;
+    }
+
+    /**
+     * Only {@link ExpressionNumber} values and target type = JDK {@link Number} is supported.
+     */
+    @Override
+    public boolean canConvert(final Object value,
+                              final Class<?> type,
+                              final ConverterContext context) {
+        return value instanceof ExpressionNumber ?
+                this.canConvertExpressionNumber((ExpressionNumber) value, type, context) :
+                this.converter.canConvert(value, type, context);
+    }
+
+    /**
+     * Asks the wrapped {@link Converter} if it can convert the {@link double} or {@link java.math.BigDecimal} value held
+     * by the {@link ExpressionNumber}.
+     */
+    private boolean canConvertExpressionNumber(final ExpressionNumber value,
+                                               final Class<?> type,
+                                               final ConverterContext context) {
+        return this.converter.canConvert(value.value(),
+                type,
+                context);
+    }
+
+
+    @Override
+    public <T> Either<T, String> convert(final Object value,
+                                         final Class<T> type,
+                                         final ConverterContext context) {
+        return value instanceof ExpressionNumber ?
+                this.convertExpressionNumber((ExpressionNumber) value, type, context) :
+                this.converter.convert(value, type, context);
+    }
+
+    private <T> Either<T, String> convertExpressionNumber(final ExpressionNumber number,
+                                                          final Class<T> type,
+                                                          final ConverterContext context) {
+        final Either<T, String> result = this.converter.convert(number.value(), type, context);
+        return result.isRight() ?
+                this.failConversion(number, type) :
+                result;
+    }
+
+    /**
+     * The assumed {@link Converter} which takes a {@link Number} and converts to some other target.
+     */
+    private final Converter converter;
+
+    @Override
+    public String toString() {
+        return ExpressionNumber.class.getSimpleName() + "|" + this.converter.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/tree/expression/ExpressionNumberConverterFromExpressionNumberNumberTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.ToStringTesting;
+import walkingkooka.convert.ConverterContext;
+import walkingkooka.convert.ConverterContexts;
+import walkingkooka.convert.ConverterTesting2;
+import walkingkooka.convert.Converters;
+import walkingkooka.datetime.DateTimeContexts;
+import walkingkooka.math.DecimalNumberContext;
+import walkingkooka.math.DecimalNumberContexts;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.text.DecimalFormat;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class ExpressionNumberConverterFromExpressionNumberNumberTest implements ConverterTesting2<ExpressionNumberConverterFromExpressionNumberNumber>,
+        ToStringTesting<ExpressionNumberConverterFromExpressionNumberNumber> {
+
+    @Test
+    public void testWithNullConverterFails() {
+        assertThrows(NullPointerException.class, () -> ExpressionNumberConverterFromExpressionNumberNumber.with(null));
+    }
+
+    @Test
+    public void testConvertFails() {
+        this.convertFails(true, String.class);
+    }
+
+    @Test
+    public void testByte() {
+        this.convertAndCheck2((byte) 123, "123");
+    }
+
+    @Test
+    public void testShort() {
+        this.convertAndCheck2((short) 123, "123");
+    }
+
+    @Test
+    public void testInteger() {
+        this.convertAndCheck2(123, "123");
+    }
+
+    @Test
+    public void testLong() {
+        this.convertAndCheck2(123L, "123");
+    }
+
+    @Test
+    public void testFloat() {
+        this.convertAndCheck2(128.5f, "128.5");
+    }
+
+    @Test
+    public void testDouble() {
+        this.convertAndCheck2(128.5, "128.5");
+    }
+
+    @Test
+    public void testBigInteger() {
+        this.convertAndCheck2(BigInteger.valueOf(123), "123");
+    }
+
+    @Test
+    public void testBigDecimal() {
+        this.convertAndCheck2(BigDecimal.valueOf(128.5), "128.5");
+    }
+
+    @Test
+    public void testExpressionNumberBigDecimal() {
+        this.convertAndCheck2(ExpressionNumber.with(BigDecimal.valueOf(128.5)), "128.5");
+    }
+
+    @Test
+    public void testExpressionNumberDouble() {
+        this.convertAndCheck2(ExpressionNumber.with(128.5), "128.5");
+    }
+
+    private void convertAndCheck2(final Object value,
+                                  final String expected) {
+        this.convertAndCheck(value, String.class, expected);
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(this.createConverter(), "ExpressionNumber|DecimalFormat.getInstance");
+    }
+
+    @Override
+    public ExpressionNumberConverterFromExpressionNumberNumber createConverter() {
+        return ExpressionNumberConverterFromExpressionNumberNumber.with(Converters.numberString(new Function<DecimalNumberContext, DecimalFormat>() {
+            @Override
+            public DecimalFormat apply(DecimalNumberContext decimalNumberContext) {
+                return (DecimalFormat) DecimalFormat.getInstance();
+            }
+
+            // want to override toString
+            @Override
+            public String toString() {
+                return "DecimalFormat.getInstance";
+            }
+        }));
+    }
+
+    @Override
+    public ConverterContext createContext() {
+        return ConverterContexts.basic(DateTimeContexts.fake(), DecimalNumberContexts.american(MathContext.DECIMAL32));
+    }
+
+    @Override
+    public Class<ExpressionNumberConverterFromExpressionNumberNumber> type() {
+        return ExpressionNumberConverterFromExpressionNumberNumber.class;
+    }
+
+    @Override
+    public String typeNamePrefix() {
+        return ExpressionNumberConverterFromExpressionNumberNumber.class.getSimpleName();
+    }
+
+    @Override
+    public String typeNameSuffix() {
+        return "";
+    }
+}
