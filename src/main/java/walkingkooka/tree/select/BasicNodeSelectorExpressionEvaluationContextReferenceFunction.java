37ceb9f0750f835f45c3abdd3dcd6f6fd747a608
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-11-03 12:01:59 +1100

    BasicNodeSelectorExpressionEvaluationContext ExpressionEvaluationContext factory function
    - Introduced BasicNodeSelectorExpressionEvaluationContextReferenceFunction
    - Added parameter checks to BasicNodeSelectorExpressionEvaluationContextTest
    - Added type parameters to BasicNodeSelectorExpressionEvaluationContextTest

---
+++ src/main/java/walkingkooka/tree/select/BasicNodeSelectorExpressionEvaluationContextReferenceFunction.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.select;
+
+import walkingkooka.naming.Name;
+import walkingkooka.tree.Node;
+import walkingkooka.tree.expression.Expression;
+import walkingkooka.tree.expression.ExpressionReference;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * A {@link Function} or lambda with a nice {@link #toString()}.
+ */
+final class BasicNodeSelectorExpressionEvaluationContextReferenceFunction<N extends Node<N, NAME, ANAME, AVALUE>,
+        NAME extends Name,
+        ANAME extends Name,
+        AVALUE> implements Function<ExpressionReference, Optional<Expression>> {
+
+    static <N extends Node<N, NAME, ANAME, AVALUE>,
+            NAME extends Name,
+            ANAME extends Name,
+            AVALUE>
+    BasicNodeSelectorExpressionEvaluationContextReferenceFunction<N, NAME, ANAME, AVALUE> with(final BasicNodeSelectorExpressionEvaluationContext<N, NAME, ANAME, AVALUE> context) {
+        return new BasicNodeSelectorExpressionEvaluationContextReferenceFunction<>(context);
+    }
+
+    private BasicNodeSelectorExpressionEvaluationContextReferenceFunction(final BasicNodeSelectorExpressionEvaluationContext<N, NAME, ANAME, AVALUE> context) {
+        super();
+        this.context = context;
+    }
+
+    @Override
+    public Optional<Expression> apply(final ExpressionReference reference) {
+        return context.reference(reference);
+    }
+
+    private final BasicNodeSelectorExpressionEvaluationContext<N, NAME, ANAME, AVALUE> context;
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
--- src/test/java/walkingkooka/tree/select/BasicNodeSelectorExpressionEvaluationContextTest.java
+++ src/test/java/walkingkooka/tree/select/BasicNodeSelectorExpressionEvaluationContextTest.java
@@ -17,13 +17,18 @@
 
 package walkingkooka.tree.select;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.map.Maps;
 import walkingkooka.convert.Converter;
 import walkingkooka.convert.ConverterContexts;
 import walkingkooka.convert.Converters;
 import walkingkooka.datetime.DateTimeContexts;
 import walkingkooka.math.DecimalNumberContext;
 import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.naming.Names;
+import walkingkooka.naming.StringName;
 import walkingkooka.tree.TestNode;
 import walkingkooka.tree.expression.Expression;
 import walkingkooka.tree.expression.ExpressionEvaluationContextTesting;
@@ -31,19 +36,35 @@ import walkingkooka.tree.expression.ExpressionEvaluationContexts;
 import walkingkooka.tree.expression.ExpressionNumberConverterContext;
 import walkingkooka.tree.expression.ExpressionNumberConverterContexts;
 import walkingkooka.tree.expression.ExpressionNumberKind;
-import walkingkooka.tree.expression.ExpressionReference;
 import walkingkooka.tree.expression.FunctionExpressionName;
+import walkingkooka.tree.select.parser.NodeSelectorAttributeName;
 
 import java.math.MathContext;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
-public final class BasicNodeSelectorExpressionEvaluationContextTest implements ExpressionEvaluationContextTesting<BasicNodeSelectorExpressionEvaluationContext> {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class BasicNodeSelectorExpressionEvaluationContextTest implements ExpressionEvaluationContextTesting<BasicNodeSelectorExpressionEvaluationContext<TestNode, StringName, StringName, Object>> {
 
     private final static ExpressionNumberKind EXPRESSION_NUMBER_KIND = ExpressionNumberKind.DEFAULT;
+
+    @BeforeEach
+    public void beforeEachTest() {
+        TestNode.clear();
+    }
+
+    @Test
+    public void testWithNullNodeFails() {
+        assertThrows(NullPointerException.class, () -> BasicNodeSelectorExpressionEvaluationContext.with(null, (r) -> ExpressionEvaluationContexts.fake()));
+    }
+
+    @Test
+    public void testWithNullContextFactoryFails() {
+        assertThrows(NullPointerException.class, () -> BasicNodeSelectorExpressionEvaluationContext.with(TestNode.with("test-node-123"), null));
+    }
 
     @Test
     public void testEvaluateTrue() {
@@ -65,29 +86,39 @@ public final class BasicNodeSelectorExpressionEvaluationContextTest implements E
         this.evaluateAndCheck(Expression.string(value), value);
     }
 
-    @Override
-    public BasicNodeSelectorExpressionEvaluationContext createContext() {
-        return BasicNodeSelectorExpressionEvaluationContext.with(null, ExpressionEvaluationContexts.basic(
-                EXPRESSION_NUMBER_KIND,
-                this.functions(),
-                this.references(),
-                this.converter(),
-                this.converterContext()
+    @Test
+    public void testReference() {
+        final String attribute = "attribute123";
+        final String value = "value123";
+        final TestNode node = TestNode.with("node123")
+                .setAttributes(Maps.of(Names.string(attribute), value));
 
-        ));
+        assertEquals(Expression.string(value),
+                this.createContext(node)
+                        .referenceOrFail(NodeSelectorAttributeName.with(attribute)));
     }
 
-    final BiFunction<FunctionExpressionName, List<Object>, Object> functions() {
+    @Override
+    public BasicNodeSelectorExpressionEvaluationContext<TestNode, StringName, StringName, Object> createContext() {
+        return this.createContext(TestNode.with("test-node-123"));
+    }
+
+    private BasicNodeSelectorExpressionEvaluationContext<TestNode, StringName, StringName, Object> createContext(final TestNode node) {
+        return BasicNodeSelectorExpressionEvaluationContext.with(node,
+                (r) -> ExpressionEvaluationContexts.basic(
+                        EXPRESSION_NUMBER_KIND,
+                        this.functions(),
+                        r,
+                        this.converter(),
+                        this.converterContext()
+                ));
+    }
+
+    private BiFunction<FunctionExpressionName, List<Object>, Object> functions() {
         return (n, p) -> {
             Objects.requireNonNull(n, "name");
             Objects.requireNonNull(p, "parameters");
             throw new IllegalArgumentException("Unknown function " + n);
-        };
-    }
-
-    private Function<ExpressionReference, Optional<Expression>> references() {
-        return r -> {
-            throw new UnsupportedOperationException();
         };
     }
 
@@ -99,8 +130,8 @@ public final class BasicNodeSelectorExpressionEvaluationContextTest implements E
     }
 
     @Override
-    public Class<BasicNodeSelectorExpressionEvaluationContext> type() {
-        return BasicNodeSelectorExpressionEvaluationContext.class;
+    public Class<BasicNodeSelectorExpressionEvaluationContext<TestNode, StringName, StringName, Object>> type() {
+        return Cast.to(BasicNodeSelectorExpressionEvaluationContext.class);
     }
 
     @Override
