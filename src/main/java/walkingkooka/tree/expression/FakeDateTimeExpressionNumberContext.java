ef5002a1f1d2954f02707e1f05f494c7886ff307
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-06-20 20:49:29 +1000

    FakeDateTimeExpressionNumberContext

---
+++ src/main/java/walkingkooka/tree/expression/FakeDateTimeExpressionNumberContext.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression;
+
+import walkingkooka.datetime.FakeDateTimeContext;
+
+import java.math.MathContext;
+
+public class FakeDateTimeExpressionNumberContext extends FakeDateTimeContext implements DateTimeExpressionNumberContext {
+    @Override
+    public String currencySymbol() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public char decimalSeparator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String exponentSymbol() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public char groupSeparator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public char percentageSymbol() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public MathContext mathContext() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public char negativeSign() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public char positiveSign() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ExpressionNumberKind expressionNumberKind() {
+        throw new UnsupportedOperationException();
+    }
+}
