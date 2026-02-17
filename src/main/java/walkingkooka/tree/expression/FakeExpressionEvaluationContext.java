66a63caa4c272c05ae2068c672e52eeb3030d22b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-02-18 09:26:01 +1100

    https://github.com/mP1/walkingkooka-locale/pull/106 LocaleContext extends CanLocaleForLanguageTag
    - https://github.com/mP1/walkingkooka-locale/pull/106
    - LocaleContext extends CanLocaleForLanguageTag

--- src/main/java/walkingkooka/tree/expression/FakeExpressionEvaluationContext.java
+++ src/main/java/walkingkooka/tree/expression/FakeExpressionEvaluationContext.java
@@ -131,6 +131,11 @@ public class FakeExpressionEvaluationContext extends FakeExpressionNumberConvert
     }
 
     @Override
+    public Optional<Locale> localeForLanguageTag(final String languageTag) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
     public Optional<String> localeText(final Locale locale) {
         throw new UnsupportedOperationException();
     }
