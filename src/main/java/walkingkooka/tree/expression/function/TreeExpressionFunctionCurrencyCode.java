32206da00492295437ffd6ada6ab2df887097551
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-06 12:56:18 +1000

    TreeExpressionFunctionCurrencyCode
    - Closes https://github.com/mP1/walkingkooka-tree/issues/1045
    - ExpressionFunction: currencyCode

---
+++ src/main/java/walkingkooka/tree/expression/function/TreeExpressionFunctionCurrencyCode.java
@@ -0,0 +1,57 @@
+package walkingkooka.tree.expression.function;
+
+import walkingkooka.Cast;
+import walkingkooka.currency.CurrencyCode;
+import walkingkooka.net.Url;
+import walkingkooka.tree.expression.ExpressionEvaluationContext;
+import walkingkooka.tree.expression.ExpressionPurityContext;
+
+import java.util.List;
+
+/**
+ * A function that accepts a {@link Url} and creates a {@link CurrencyCode}.
+ */
+final class TreeExpressionFunctionCurrencyCode<C extends ExpressionEvaluationContext> extends TreeExpressionFunction<CurrencyCode, C> {
+
+    static <C extends ExpressionEvaluationContext> TreeExpressionFunctionCurrencyCode<C> instance() {
+        return Cast.to(INSTANCE);
+    }
+
+    /**
+     * Singleton
+     */
+    private final static TreeExpressionFunctionCurrencyCode<ExpressionEvaluationContext> INSTANCE = new TreeExpressionFunctionCurrencyCode<>();
+
+    private TreeExpressionFunctionCurrencyCode() {
+        super("currencyCode");
+    }
+
+    @Override
+    public List<ExpressionFunctionParameter<?>> parameters(final int count) {
+        return PARAMETERS;
+    }
+
+    final static ExpressionFunctionParameter<CurrencyCode> CURRENCY_CODE = ExpressionFunctionParameterName.with("currencyCode")
+        .required(CurrencyCode.class)
+        .setKinds(ExpressionFunctionParameterKind.CONVERT_EVALUATE);
+
+    private final static List<ExpressionFunctionParameter<?>> PARAMETERS = ExpressionFunctionParameter.list(
+        CURRENCY_CODE
+    );
+
+    @Override
+    public Class<CurrencyCode> returnType() {
+        return CurrencyCode.class;
+    }
+
+    @Override
+    public boolean isPure(final ExpressionPurityContext context) {
+        return true;
+    }
+
+    @Override
+    public CurrencyCode apply(final List<Object> parameters,
+                              final C context) {
+        return CURRENCY_CODE.getOrFail(parameters, 0);
+    }
+}
